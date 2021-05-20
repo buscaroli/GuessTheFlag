743d710b6d511a35370ed391a8ac5727a8b248f9
mabus <m.buscaroli@outlook.com>
2021-05-20 22:12:29 +0100

    "Basic Game completed, need to figure out how to reset variables after Alert is dismissed."

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -7,10 +7,82 @@
 
 import SwiftUI
 
+    
 struct ContentView: View {
+    @State private var countries: [String] = ["Italy", "France", "Germany", "Spain", "UK", "Estonia"].shuffled()
+    @State private var correct = Int.random(in: 0 ... 2)
+    @State private var score: Int = 0
+    @State private var questionsRemaining: Int = 6
+    @State private var gameOver: Bool = false
+
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.white, .black]), startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea(edges: .all)
+            VStack(spacing: 30) {
+                Text("Pick the flag of:")
+                    .foregroundColor(.primary)
+                    .font(.title)
+                Text(countries[correct])
+                    .foregroundColor(.primary)
+                    .font(.title)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+            
+                VStack {
+                    ForEach(0 ..< 3) { flag in
+                        Button(action: {
+                            checkAnswer(flag)
+                            randomizeFlag()
+                        }) {
+                            Image(countries[flag])
+                                .renderingMode(.original)
+                                .clipShape(Capsule())
+                                .padding()
+                                .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                        }
+                        .alert(isPresented: $gameOver) {
+                            Alert(title: Text("GAME OVER"),
+                                  message: Text("You Scored \(score)"))
+                        }
+                    }
+                }
+                Spacer()
+                
+                Text("Score: \(score) point")
+                    .font(.largeTitle)
+                    .foregroundColor(.yellow)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                
+                Spacer()
+            }
+        }
+        
+    }
+    
+    func checkAnswer(_ flag: Int) {
+        if flag == correct {
+            score += 1
+        } else {
+            if (score > 0) {
+                score -= 1
+            }
+        }
+        countries.shuffle()
+        questionsRemaining -= 1
+        if (questionsRemaining <= 0) {
+            gameOver = true
+        }
+    }
+    
+    func randomizeFlag() {
+        correct = Int.random(in: 0 ... 2)
+    }
+    
+    func resetGame() {
+        countries.shuffle()
+        score = 0
+        questionsRemaining = 6
     }
 }
 
